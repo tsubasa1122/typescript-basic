8b1a74a5caf82373ecb76b73e44f390110f57a97
tsubasa1122 <nakanot@open8.com>
2020-03-20 22:09:37 +0900

    learn array

---
+++ src/number-string.ts
@@ -0,0 +1,14 @@
+export { };
+
+let year: number = 1976;
+// year = false;
+
+console.log(year);
+
+let age: number = 0x2b;
+
+console.log(age);
+
+let name: string = "Ham";
+// name = 2
+console.log(name);
