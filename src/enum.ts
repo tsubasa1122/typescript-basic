34817cf23c33711c8ed88f7bc1b604889a1ae911
tsubasa1122 <nakanot@open8.com>
2020-03-21 10:28:16 +0900

    learn enum

---
+++ src/enum.ts
@@ -0,0 +1,37 @@
+export { };
+
+enum Months {
+  Janumary = 1,
+  Feburary,
+  March,
+  April,
+  May,
+  Jun,
+  July,
+  August,
+  Septemper,
+  October,
+  November,
+  December
+}
+
+Months.Janumary;
+Months.Feburary;
+
+enum COLORS {
+  RED = '#FF000',
+  WHITE = '#FFFFFF',
+  GREEN = '#008000',
+  BLUE = '#0000FF',
+  BLAck = '#000000',
+}
+
+let green = COLORS.GREEN;
+
+console.log({ green });
+
+enum COLORS {
+  YELLOW = '#FFFF00',
+};
+
+COLORS.YELLOW;
