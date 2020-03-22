eb11d905430a45b4e1d556afc005012832944fcb
tsubasa1122 <nakanot@open8.com>
2020-03-23 01:06:08 +0900

    learn rest-parameters

---
+++ src/rest-parameters.ts
@@ -0,0 +1,13 @@
+export { };
+
+const reducer = (accumulator: number, currentValue: number) => {
+  return accumulator + currentValue;
+};
+
+const sum = (...values: number[]): number => {
+  return values.reduce(reducer);
+};
+
+console.log(sum(1, 2, 3, 4, 5));
+
+[1, 2, 3, 4, 5].reduce(reducer);
