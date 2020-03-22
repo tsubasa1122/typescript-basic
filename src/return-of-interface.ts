9babaced628411f66b41ae375944c6596ebdffd8
tsubasa1122 <nakanot@open8.com>
2020-03-23 03:56:54 +0900

    learn interface

---
+++ src/return-of-interface.ts
@@ -0,0 +1,23 @@
+export { };
+
+class Mahoutsukai { }
+class Souryo { }
+
+class Taro extends Mahoutsukai { }
+
+interface Kenja {
+  ionazun(): void;
+}
+interface Senshi {
+  kougeki(): void;
+}
+
+class Jiro implements Kenja, Senshi {
+  ionazun(): void {
+    console.log('ionazun');
+  }
+
+  kougeki(): void {
+    console.log('kougeki');
+  }
+}
